3b787e2a51ff79208d43acc232b20b0a607b50f8
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-17 13:05:58 +0100

    added the service to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodosContainerComponent } from './components/todos-container/todos-container.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosContainerComponent
   ],
   imports: [
     BrowserModule,
