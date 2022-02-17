400bbde7673d295aa5d76dd5a5741d9e0e5c9255
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-17 14:38:21 +0100

    updated service with all the backend endpoints

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { ITodo } from "../../../interfaces/ITodo";
+
+const baseUrl = 'http://localhost:5000/api/todos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listTodos():Observable<ITodo[]>{
+    return this.http.get<ITodo[]>(baseUrl)
+  }
+
+  getTodo(id:string):Observable<ITodo>{
+    return this.http.get<ITodo>(baseUrl.concat('/',id))
+  }
+
+  create(todo:ITodo):Observable<any> {
+    return this.http.post(baseUrl,todo)
+  }
+
+  delete(id:string):Observable<any>{
+    return this.http.delete(baseUrl.concat('/',id))
+  }
+
+  update(todo:ITodo):Observable<any>{
+    return this.http.put(baseUrl,todo)
+  }
 }
