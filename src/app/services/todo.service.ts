9c40958a9c14247ab2d776001ad81ed0a26e6115
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-02-24 05:44:47 -0800

    added a route to get users

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { ITodo } from "../../../interfaces/ITodo";
+import { IUser } from 'interfaces/IUser';
 
-const baseUrl = 'http://localhost:5000/api/todos';
+const baseUrl = 'http://localhost:5000/api';
 
 @Injectable({
   providedIn: 'root'
@@ -13,24 +14,28 @@ export class TodoService {
   constructor(private http: HttpClient) { }
 
   listTodos():Observable<ITodo[]>{
-    return this.http.get<ITodo[]>(baseUrl)
+    return this.http.get<ITodo[]>(`${baseUrl}/todos`)
   }
 
   getTodo(id:string):Observable<ITodo>{
-    return this.http.get<ITodo>(baseUrl.concat('/',id))
+    return this.http.get<ITodo>(`${baseUrl}/todos/${id}`)
   }
 
   create(todo:ITodo):Observable<any> {
     console.log("------------------")
     console.log(todo)
-    return this.http.post(baseUrl,todo)
+    return this.http.post(`${baseUrl}/todos`,todo)
   }
 
   delete(id:string):Observable<any>{
-    return this.http.delete(baseUrl.concat('/',id))
+    return this.http.delete(`${baseUrl}/todos/${id}`)
   }
 
   update(todo:ITodo):Observable<any>{
-    return this.http.put(baseUrl,todo)
+    return this.http.put(`${baseUrl}/todos`,todo)
+  }
+
+  getUsers():Observable<IUser[]>{
+    return this.http.get<IUser[]>(`${baseUrl}/users`)
   }
 }
